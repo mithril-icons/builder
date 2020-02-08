9bcf0f73d2f2d34d9c5fa98de3328f1ce9044e5d
Michal Kijowski <contact@michalkijowski.pl>
2020-02-08 22:56:41 +0100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,9 @@ module.exports = {
   extends: "standard-with-typescript",
   parserOptions: {
     project: "./tsconfig.json"
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/restrict-template-expressions": ["error", { allowNumber: true }]
   }
 };
